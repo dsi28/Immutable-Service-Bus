cb2bdd05139e0a6ccc0e4f42e802f3fa2a82816e
dsi28 <dsi28@hotmail.com>
2024-02-18 15:49:07 -0600

    added http trigger function that sends message to service bus

--- function_app.py
+++ function_app.py
@@ -1,13 +1,16 @@
 import azure.functions as func
 import logging
 from azure.storage.blob import BlobServiceClient
+import asyncio
+from azure.servicebus.aio import ServiceBusClient
+from azure.servicebus import ServiceBusMessage
 import datetime
 import os
 
 
 app = func.FunctionApp()
 
-@app.service_bus_queue_trigger(arg_name="sbmessage", queue_name="mysbqueue",
+@app.service_bus_queue_trigger(arg_name="sbmessage", queue_name=os.getenv('SERVICE_BUS_QUEUE'),
                                connection="daizquieSBNS_SERVICEBUS") 
 def servicebus_queue_trigger(sbmessage: func.ServiceBusMessage, context:func.Context):
     message_text =  sbmessage.get_body().decode("utf-8")
@@ -31,7 +34,8 @@ def aquire_lock(message_id, invocation_id):
     blob_service_client = BlobServiceClient.from_connection_string(os.getenv("AzureWebJobsStorage"))
 
     # temp test setting a static message id
-    message_id = "1234"
+    if os.getenv('USE_STATIC_MESSAGE_ID'):
+        message_id = "1234"
     blob_name = f"lock-{message_id}.lock"
 
     # get blob even if its not created for the lock file name
@@ -53,6 +57,7 @@ def aquire_lock(message_id, invocation_id):
             raise Exception(ex_message)
         else:
             # update lock and owner since the lock is expired in order to give this function execution the lock
+            logging.warning('lock is expired')
             new_lock_time = new_lock(5)
             blob_client.set_blob_metadata(metadata={"ExpirationTime":f"{new_lock_time}", "Owner":f"{invocation_id}"})
             blob_properties = blob_client.get_blob_properties()
@@ -73,3 +78,45 @@ def aquire_lock(message_id, invocation_id):
 
     
     
+# http trigger function that sends message to service bus queue
+@app.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
+async def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    message_data = req.params.get('message')
+    if not message_data:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            message_data = req_body.get('message')
+
+    if message_data:
+        # send message to service bus. log message id here. 
+        try:
+            logging.warning('sending')
+            await send_service_bus_message(message_data)
+            return func.HttpResponse(f"Message sent to service bus queue. This HTTP triggered function executed successfully.")
+        except Exception as ex:
+            logging.error(str(ex))
+            return func.HttpResponse(f"There was an exception when sending message to service bus. This HTTP triggered function executed successfully.", status_code=500)            
+    else:
+        return func.HttpResponse(
+             "no message data was sent with this request. no message sent to service bus",
+             status_code=400
+        )
+    
+
+
+# create a Service Bus client using the connection string
+async def send_service_bus_message(m_data):
+    async with ServiceBusClient.from_connection_string(conn_str=os.getenv('daizquieSBNS_SERVICEBUS'), 
+                                                       logging_enable=True) as servicebus_client:
+        logging.warning('sending 2')
+        # Get a Queue Sender object to send messages to the queue
+        sender = servicebus_client.get_queue_sender(queue_name=os.getenv('SERVICE_BUS_QUEUE'))
+        async with sender:
+            # Send one message
+            await sender.send_messages(ServiceBusMessage(m_data))
+            logging.warning("Sent a single message")
